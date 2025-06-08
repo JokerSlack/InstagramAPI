8e9de2b6fd992f843ae7d3b5a82efc5838c334e4
jokerSlack <jorgeguimaraes.ware@gmail.com>
2025-06-08 13:57:08 -0300

    Session

--- config.py
+++ config.py
@@ -1,12 +1,31 @@
-import configParser
+import configparser
+import os
 
-config = configparser.ConfigParser()
-try:
-	with open("config.ini", "r") as f:
-		print("Arquivo de configuração encontrado!")
-	config.read("config.ini")
-except FileNotFoundError:
-	print("Arquivo de configuração não encontrado!")
+# Classe que fará esta lib funcionar
+class Config:
+    def __init__(self, configFileName="config.ini", username="default", password="default") -> object:
+        self.configFileName = configFileName
+        self.username = username
+        self.password = password
 
-username = config["instagram"]["username"]
-password = config["instagram"]["password"]
+    def config(self):
+        if os.path.exists("./" + self.configFileName):
+            print("Arquivo de configuração encontrado!")
+        else:
+            if input("Você quer criar um arquivo de configuração[Y/n]: ").upper() == "Y":
+                print("Inicializando criação de arquivo...")
+                username = input("Username: ")
+                password = input("Password: ")
+                config = configparser.ConfigParser()
+                config["instagram"] = {'username': username, 'password': password}
+                with open('config.ini', 'a') as f:
+                    config.write(f)
+                print("Arquivo de configuração criado!")
+            else:
+                raise FileNotFoundError("Arquivo de configuração não foi encontrado!")
+
+        parser = configparser.ConfigParser()
+        parser.read(self.configFileName)
+        self.username = parser["instagram"]["username"]
+        self.password = parser["instagram"]["password"]
+        print("Configuração concluída!")
--- main.py
+++ main.py
@@ -1,19 +1,56 @@
 from instagrapi import Client
-import configparser
+from config import Config
+import api
+import os
+
+# Config
+cfg = Config()
+cfg.config()
 
 # Login
-cl = Client()
-cl.login("jorge13.js", "c0id23;g")
+client = Client()
+session_file = f"{username}_session.json"
 
-# Listar últimas conversas
-threads = cl.direct_threads(amount=5)
-for i, thread in enumerate(threads):
-    print(f"{i+1}. {thread.users[0].username}: {thread.messages[0].text}")
+# Criação de sessão
+if os.path.exists(session_file):
+    client.load_settings(session_file)
+    try:
+        client.login(username, password)
+    except Exception:
+        print("⚠️ Sessão antiga inválida. Logando do zero...")
+        client.set_settings({})
+        client.login(cfg.username, cfg.password)
+        client.dump_settings(session_file)
+else:
+    client.login(cfg.username, cfg.password)
+    client.dump_settings(session_file)
+print("✅ Login feito com sucesso!")
 
-# Selecionar uma conversa e mandar mensagem
-index = int(input("Selecione a conversa (número): ")) - 1
-thread = threads[index]
+# Selecione o que quer fazer
 
-msg = input("Digite a mensagem: ")
-cl.direct_send(msg, [thread.users[0].pk])
-print("Mensagem enviada.")
+while True:
+    threads = client.direct_threads()
+    for thread in threads:
+        if thread.unseen_count > 0:
+            user = thread.users[0].username
+            print(f"Nova mensagem de {user}")
+    print("""
+        1 - Conversar
+        2 - Pesquisar
+    """)
+    menu = int(input("Selecione o que quer fazer:"))
+    if menu == 1:
+        threads = client.direct_threads(amount=5) # Mostrar conversas
+        for i, thread in enumerate(threads):
+            print(f"{i+1}. {thread.users[0].username}: {thread.messages[0].text}")
+        index = int(input("Selecione a conversa (número): ")) - 1
+        thread = threads[index]
+        messages = client.direct_messages(thread, amount=20) # Mensagens da conversa escolhida
+        print(f"\n🗨️ Histórico de conversa com @:\n")
+        for msg in reversed(messages):
+            author = client.user_info(msg.user_id).username
+            print(f"{author}: {msg.text}")
+
+#msg = input("Digite a mensagem: ")
+#client.direct_send(msg, [thread.users[0].pk])
+#print("Mensagem enviada.")
